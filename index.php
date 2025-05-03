517920f1a0efd522850fedc3b3264374b543871c
luchoNeiman <neimanlucho@gmail.com>
2025-05-03 18:03:23 -0300

    arrancando con maquetado de productos

--- index.php
+++ index.php
@@ -35,8 +35,8 @@ $rutaConfig = $rutas[$seccion];
     <title><?= $rutaConfig['titulo']; ?> - Otaku Mania</title><!-- Muestra el título de la página dinámica basado en la sección actual -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="assets/css/estilos.css">
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
-    <script defer src="assets/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <script defer src="assets/js/bootstrap.bundle.min.js"></script>
 
 </head>
 
--- views/productos.php
+++ views/productos.php
@@ -1,32 +1,32 @@
 <?php
 /* require_once __DIR__ . '/../bibliotecas/productos.php'; */
- require_once __DIR__ . '/../classes/Producto.php';
+require_once __DIR__ . '/../classes/Producto.php';
 
 /* $productos = obtenerproductos(); */
 // Esta línea crea una nueva instancia de la clase Producto y llama al método todosProductos() para obtener la lista de todos los productos.
 $productos = (new Producto)->todosProductos();
 ?>
-<section class="news">
-    <div>
-        <h1>Últimas productos</h1>
-        <p class="news-lead">Qué está pasando.</p>
-    </div>
-    <div class="news-list">
-        <?php
-        foreach ($productos as $producto):
-        ?>
-            <div class="card">
-                <article class="news-item">
-                    <div class="news-item_content card-body">
-                        <a href="index.php?seccion=detalle-producto&id=<?= $producto->producto_id;?>">
-                            <h2><?= $producto->titulo;?></h2>
-                        </a>
-                        <p><?= $producto->descripcion;?></p>
-                    </div>
-                </article>
-            </div>
-        <?php
-        endforeach;
-        ?>
+<section class="productos">
+    <div class="container">
+        <h1>Nuestros Productos</h1>
+        <p class="lead">Los mejores artículos de tus series favoritas</p>
+        <div class="row">
+            <?php
+            // Este foreach simula productos, reemplazalo con tu lógica real más adelante
+            foreach ($productos as $producto):
+            ?>
+                <div class="col-12 col-sm-6 col-md-3 mb-4">
+                    <article class="card h-100">
+                        <img src="assets/imgs/products/<?= htmlspecialchars($producto->imagen); ?>" class="card-img-top" alt="<?= htmlspecialchars($producto->imagen_descripcion); ?>">
+                        <div class="card-body d-flex flex-column">
+                            <h2 class="card-title fs-5"><?= htmlspecialchars($producto->titulo); ?></h2>
+                            <p class="card-text"><?= htmlspecialchars($producto->descripcion); ?></p>
+                            <p class="card-text fw-bold text-primary mt-auto">$<?= number_format($producto->precio, 2, ',', '.'); ?></p>
+                            <a href="index.php?seccion=detalle-producto&id=<?= $producto->producto_id; ?>" class="btn btn-outline-dark mt-2">Ver más</a>
+                        </div>
+                    </article>
+                </div>
+            <?php endforeach; ?>
+        </div>
     </div>
 </section>
