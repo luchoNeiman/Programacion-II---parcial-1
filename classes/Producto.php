7ee6d51fd5b71293e357952fc65d9701bc52babd
luchoNeiman <neimanlucho@gmail.com>
2025-06-08 15:42:33 -0300

    getter y setter

--- classes/Producto.php
+++ classes/Producto.php
@@ -1,11 +1,12 @@
 <?php
-const PRODUCTOS_JSON = 'productos.json';
+require_once __DIR__ . '/DBConexion.php';
+// const PRODUCTOS_JSON = 'productos.json';
 
 class Producto
 {
     private int $producto_id = 0;
-    private int $usuario_id = 0;
-    private string $fecha_igreso = '';
+    private int $usuario_fk = 0;
+    private string $fecha_ingreso = "";
     private string $titulo = "";
     private string $descripcion = "";
     private float $precio = 0.0;
@@ -24,37 +25,32 @@ class Producto
      */
     public function cargarDatosArray(array $data): void
     {
-        $this->producto_id = $data['producto_id'];
-        $this->titulo = $data['titulo'];
-        $this->descripcion = $data['descripcion'];
-        $this->precio = $data['precio'];
-        $this->imagen = $data['imagen'];
-        $this->imagen_descripcion = $data['imagen_descripcion'];
-        $this->franquicia = $data['franquicia'];
-        $this->categoria = $data['categoria'];   
-        $this->caracteristicas = $data['caracteristicas'];
+        $this->producto_id              = $data['id'];
+        $this->titulo                   = $data['titulo'];
+        $this->descripcion              = $data['descripcion'];
+        $this->precio                   = $data['precio'];
+        $this->imagen                   = $data['imagen'];
+        $this->imagen_descripcion       = $data['imagen_descripcion'];
+        $this->franquicia               = $data['franquicia'];
+        $this->categoria                = $data['categoria'];
+        $this->caracteristicas          = $data['caracteristicas'];
     }
 
     /**
      * Recupera y devuelve todos los productos desde un archivo JSON.
-     *
-     * @return Producto[] Lista de objetos Producto que representan todos los productos disponibles.
+     * @return self[] Lista de objetos Producto que representan todos los productos disponibles.
      */
     public function todosProductos(): array
     {
-        $productoJson = json_decode(file_get_contents(__DIR__ . '/../data/' . PRODUCTOS_JSON), true);
-        // Leer el archivo JSON que contiene todos los productos y convertir su contenido en un array asociativo.
+        // Vamos a traer los productos de la base de datos.
+        $db = (new DBConexion)->getConexion();
 
-        $productos = []; // array donde se almacena todos los objetos Producto.
+        $consulta = "SELECT * FROM productos";
+        $stmt = $db->prepare($consulta);
+        $stmt->execute();
+        $stmt->setFetchMode(PDO::FETCH_CLASS, self::class);
 
-        foreach ($productoJson as $unProductoJson) {
-            $producto = new self; // Crear una nueva instancia de la clase Producto.
-            $producto->cargarDatosArray($unProductoJson); // Cargar los datos del producto.
-
-            $productos[] = $producto; // Añadir el objeto Producto al array.
-        }
-
-        return $productos;
+        return $stmt->fetchAll();
     }
 
     /**
@@ -64,20 +60,154 @@ class Producto
      *
      * @return self|null Devuelve una instancia de Producto si el ID coincide, o null si no se encuentra.
      */
+
     public function porId(int $id): ?self
     {
-        // Busca y devuelve un producto específico por su ID, o null si no existe.
-        $productos = $this->todosProductos(); // Obtiene todos los productos.
+        // Traemos el producto desde la base de datos.
+        $db = (new DBConexion)->getConexion();
+        $consulta = "SELECT * FROM productos
+                    WHERE producto_id = ?";
 
-        foreach ($productos as $unProducto) {
-            if ($unProducto->producto_id == $id) {
-                // Si el ID coincide con el buscado.
-                return $unProducto;
-            }
-        }
-        return null;
+        $stmt = $db->prepare($consulta);
+        $stmt->execute([$id]);
+
+        $stmt->setFetchMode(PDO::FETCH_CLASS, self::class);
+
+        $noticia = $stmt->fetch();
+
+        if (!$noticia) return null;
+
+        return $noticia;
     }
 
+
+    public function getProductoId(): int
+    {
+        return $this->producto_id;
+    }
+
+    public function setProductoId(int $producto_id): void
+    {
+        $this->producto_id = $producto_id;
+    }
+
+    public function getUsuarioFk(): int
+    {
+        return $this->usuario_fk;
+    }
+
+    public function setUsuarioFk(int $usuario_fk): void
+    {
+        $this->usuario_fk = $usuario_fk;
+    }
+
+    public function getFechaIngreso(): string
+    {
+        return $this->fecha_ingreso;
+    }
+
+    public function setFechaIngreso(string $fecha_ingreso): void
+    {
+        $this->fecha_ingreso = $fecha_ingreso;
+    }
+
+    public function getTitulo(): string
+    {
+        return $this->titulo;
+    }
+
+    public function setTitulo(string $titulo): void
+    {
+        $this->titulo = $titulo;
+    }
+
+    public function getDescripcion(): string
+    {
+        return $this->descripcion;
+    }
+
+    public function setDescripcion(string $descripcion): void
+    {
+        $this->descripcion = $descripcion;
+    }
+
+    public function getPrecio(): int
+    {
+        return $this->precio;
+    }
+
+    public function setPrecio(string $precio): void
+    {
+        $this->precio = $precio;
+    }
+
+    public function getImagen(): ?string
+    {
+        return $this->imagen;
+    }
+
+    public function setImagen(?string $imagen): void
+    {
+        $this->imagen = $imagen;
+    }
+
+    public function getImagenDescripcion(): ?string
+    {
+        return $this->imagen_descripcion;
+    }
+
+    public function setImagenDescripcion(?string $imagen_descripcion): void
+    {
+        $this->imagen_descripcion = $imagen_descripcion;
+    }
+
+    public function getFranquicia(): string
+    {
+        return $this->franquicia;
+    }
+
+    public function setFranquicia(string $franquicia): void
+    {
+        $this->franquicia = $franquicia;
+    }
+
+    public function getCategoria(): string
+    {
+        return $this->categoria;
+    }
+
+    public function setCategoria(string $categoria): void
+    {
+        $this->categoria = $categoria;
+    }
+
+    public function getCaracteristicas(): string
+    {
+        return $this->caracteristicas;
+    }
+
+    public function setCaracteristicas(string $caracteristicas): void
+    {
+        $this->caracteristicas = $caracteristicas;
+    }
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     /**
      * Recupera productos filtrados por tipo de edición (máximo 4 productos).
      *
@@ -109,6 +239,4 @@ class Producto
         // Convertir a array indexado y obtener solo los primeros 4 elementos
         return array_slice(array_values($filtrados), 0, 4);
     }
-
-
 }
