bad724e59718768fe9b691c55cb5aabcf6c7b10e
ricardo garcia <ricardogarci4@gmail.com>
2025-06-15 01:48:05 -0300

    pagina de error

---
+++ admin/views/404.php
@@ -0,0 +1,11 @@
+<section class="container text-center py-5">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <img src="../assets/imgs/validaciones/error-404-admin.png" alt="Error 404 - Página no encontrada" class="img-fluid w-50 mb-4">
+            <p class="fs-4 mb-4">Lo sentimos, la página que estás buscando no existe.</p>
+            <a href="../index.php?seccion=home" class="btn btn-light">
+                <i class="bi bi-house-door me-2 text-dark"></i>Volver al inicio
+            </a>
+        </div>
+    </div>
+</section>
