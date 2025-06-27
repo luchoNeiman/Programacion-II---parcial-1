d52a8ccaea12feb6c54e630c7c7b2aa74e44033c
ricardo garcia <ricardogarci4@gmail.com>
2025-06-27 00:22:31 -0300

    fix

--- admin/views/404.php
+++ admin/views/404.php
@@ -4,7 +4,7 @@
             <img src="../assets/imgs/validaciones/error-404-admin.png" alt="Error 404 - Página no encontrada" class="img-fluid w-50 mb-4">
             <p class="fs-4 mb-4 fw-bold "><i class="bi bi-exclamation-triangle-fill me-2"></i>Lo sentimos, la
                 página que estás buscando no existe.</p>
-            <a href="../index.php?seccion=dashboard" class="btn btn-light">
+            <a href="index.php?seccion=dashboard" class="btn btn-light">
                 <i class="bi bi-house-door me-2 text-dark"></i>Volver al panel
             </a>
         </div>
