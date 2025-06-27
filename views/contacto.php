dcf48832ba1f332acdf511da3371d40863f00cec
ricardo garcia <ricardogarci4@gmail.com>
2025-06-27 00:25:54 -0300

    verificado contacto

--- views/contacto.php
+++ views/contacto.php
@@ -18,24 +18,24 @@
 
         <div class="col-md-6 card shadow mb-3">
             <div class="card-body">
-                <form method="POST" action="index.php?seccion=procesar-form" class="mx-auto texto-color">
+                <form method="POST" action="index.php?seccion=procesar-form" class="mx-auto">
                     <div class="mb-3">
-                        <label for="nombre" class="form-label">Nombre completo</label>
+                        <label for="nombre" class="form-label fw-bold">Nombre completo</label>
                         <input type="text" class="form-control border-secondary" id="nombre" name="nombre" required>
                     </div>
                     <div class="mb-3">
-                        <label for="email" class="form-label">Correo electrónico</label>
+                        <label for="email" class="form-label fw-bold">Correo electrónico</label>
                         <input type="email" class="form-control border-secondary" id="email" name="email" required>
                     </div>
             
+
+                    <div class="mb-3">
+                        <label for="mensaje" class="form-label fw-bold">Mensaje</label>
+                        <textarea class="form-control border-secondary" id="mensaje" name="mensaje" rows="5"></textarea>
+                    </div>
                     <div class="mb-3 form-check">
                         <input type="checkbox" class="form-check-input border-secondary" id="check" name="check">
                         <label class="form-check-label text-dark" for="check">Quiero recibir notificaciones</label>
-                    </div>
-            
-                    <div class="mb-3">
-                        <label for="mensaje" class="form-label">Mensaje</label>
-                        <textarea class="form-control border-secondary" id="mensaje" name="mensaje" rows="5"></textarea>
                     </div>
                     <div class="d-flex justify-content-center align-items-center">
                         <button type="submit" class="btn btn-dark ">Enviar</button>
