0706e70c287031a4896118ac0b322f9c4bcdcb91
ricardo garcia <ricardogarci4@gmail.com>
2025-06-27 00:31:58 -0300

    verificado iniciar sesion

--- admin/acciones/iniciarSesion.php
+++ admin/acciones/iniciarSesion.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__ . '/../../bootstrap/autoload.php';
-session_start();
+require_once __DIR__ . '/../../bootstrap/init.php';
+
 
 // Capturamos los datos.
 $email      = $_POST['email'];
@@ -9,7 +9,7 @@ $password   = $_POST['password'];
 $autenticacion = new Autenticacion;
 
 if(!$autenticacion->intentarIngresar($email, $password)) {
-    $_SESSION['feedback_error'] = "Las credenciales ingresadas no coinciden con nuestros registros.";
+    $_SESSION['feedback_error'] = "❌Las credenciales ingresadas no coinciden con nuestros registros.";
     $_SESSION['data_vieja'] = $_POST;
     header('Location: ../index.php');
     exit;
